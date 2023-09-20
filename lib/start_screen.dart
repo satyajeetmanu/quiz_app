9cd5f7682461789bfacb2a885e3b9990a953c997
Satyajeet Gaurav <satyajeetmanu@gmail.com>
2023-09-20 16:34:04 +0530

    added icon to button and transparency to image

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -12,6 +12,7 @@ class StartScreen extends StatelessWidget {
           Image.asset(
             'assets/images/quiz-logo.png',
             width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
           ),
           const SizedBox(
             height: 80,
@@ -26,9 +27,13 @@ class StartScreen extends StatelessWidget {
           const SizedBox(
             height: 30,
           ),
-          OutlinedButton(
+          OutlinedButton.icon(
             onPressed: () {},
-            child: const Text(
+            icon: const Icon(
+              Icons.arrow_circle_right_outlined,
+              color: Colors.white,
+            ),
+            label: const Text(
               'Start Quiz',
               style: TextStyle(
                 color: Colors.white,
